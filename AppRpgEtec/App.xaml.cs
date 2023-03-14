be234d66be406a6c0c281c478013cc12804947c4
Daniel-DaMata <daniel.soares.damata@gmail.com>
2023-03-13 22:51:03 -0300

    AppRpg

---
+++ AppRpgEtec/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppRpgEtec;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		MainPage = new NavigationPage(new Views.Usuarios.LoginView());
+	}
+}
